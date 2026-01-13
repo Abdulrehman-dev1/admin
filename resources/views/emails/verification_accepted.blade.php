f3434790e2b165a0a624a1debc0d7fa783547ed0
Abdurrehman <Abdulrehman-dev1>
2026-01-13 16:30:27 +0500

    new

--- resources/views/emails/verification_accepted.blade.php
+++ resources/views/emails/verification_accepted.blade.php
@@ -1,2 +1,14 @@
+@component('mail::message')
+# Congratulations!
+
+Your identity verification for **{{ $name }}** has been **approved**.
+
+You can now participate in auctions and use all features of {{ config('app.name') }}.
+
+@component('mail::button', ['url' => config('app.frontend_url') . '/userDashboard'])
+Go to Dashboard
+@endcomponent
+
+Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
--- resources/views/emails/verification_declined.blade.php
+++ resources/views/emails/verification_declined.blade.php
@@ -1,13 +1,41 @@
-@php
-  $computed = 'https://xpertbid.com/verify/' . $verification->id;
-@endphp
-<p>
-  <a href="{{ $computed }}" class="btn btn-primary">
-    Resubmit Your Verification
-  </a>
-</p>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Verification Declined</title>
+</head>
+<body>
+    @php
+      $computed = 'https://xpertbid.com/account?tab=identity_verification';
+    @endphp
 
-<p>Thank you,<br>{{ config('app.name') }}</p>
+    <h1>Identity Verification Declined</h1>
+
+    <p>Dear {{ $user->name ?? 'User' }},</p>
+
+    <p>
+        Unfortunately, your individual identity verification was declined.
+        <br>
+        <strong>Reason:</strong> {{ $declineReason }}
+    </p>
+
+    <p>
+        Please review the reason and resubmit your documents.
+    </p>
+
+    <p>
+      <a href="{{ $computed }}" style="
+          display:inline-block;
+          padding:10px 20px;
+          background-color:#e74c3c;
+          color:#fff;
+          text-decoration:none;
+          border-radius:4px;
+        ">
+        Resubmit Verification
+      </a>
+    </p>
+
+    <p>Thank you,<br>{{ config('app.name') }}</p>
 </body>
-
 </html>
